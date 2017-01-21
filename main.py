5fc1e81f188134a7456cd5a835721714c2ed2190
Brett O'Daniell <brettlod@yahoo.com>
2017-01-20 23:32:06 -0600

    implemented lucky number, fortune list, random generating and repeat button.

--- main.py
+++ main.py
@@ -15,10 +15,47 @@
 # limitations under the License.
 #
 import webapp2
+import random
+
+def getRandomFortune():
+# list of possilbe fortune
+    fortunes = [
+        "Smile Like You Mean It.",
+        "Anger leads to fear. Fear leads to the darkside.",
+        "There is no try. You either do or do not.",
+        "Never dance with the devil in the pail moonlight.",
+        "Today will be better than yesterday, but no better than tomorrow...",
+        "Never tug on Superman's cape.",
+        "Never spit into the wind.",
+        "Don't take the mask off the Lone Ranger.",
+        "You don't mess around with Jim.",
+        "Take it easy.",
+        ]
+# randomly select one of the fortunes
+    length=int(len(fortunes))
+    index = random.randint(0, length-1)
+
+    return fortunes[index]
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!!!!!!')
+        header = "<h1>Fortune Cookie</h1>"
+
+        fortune = "<strong>" + getRandomFortune() + "</strong>"
+        fortune_sentence = "Your fortune: " + fortune
+        fortune_paragraph = "<p>" + fortune_sentence + "</p>"
+
+        lucky_Number = "<strong>" + str(random.randint(1,100)) + "</strong>"
+        number_sentence = 'Your lucky number: ' + str(lucky_Number)
+        number_paragraph  = "<p>" + number_sentence + "</p>"
+
+        cookie_again_button = "<a href='.'><button>Another cookie please :)</button></a>"
+
+        content = header + fortune_paragraph + number_paragraph + cookie_again_button
+        self.response.write(content)
+
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
